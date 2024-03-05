e6d20e03c0f907c3ceb16981adedf099daf0c0ee
basil-byui <khu23004@byui.edu>
2024-03-05 15:15:49 +0200

    completed prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name?");
+        String name = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        String lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname},{name},{lastname}");
+
+
+
     }
 }
