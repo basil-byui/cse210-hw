d37f97848cf06151217f7e508b3e9c02896e7a8f
basil-byui <khu23004@byui.edu>
2024-03-10 05:33:56 +0200

    completed resume activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Job
+{
+    public string_jobTitle;
+    public string_company;
+  
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company})")
+
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Sumsung"
+
+
+        Job job2 = new Job()
+        job2.string_jobTitle = "Manager";
+        job2._company = "Apple"
+
+        Resume myResume = newResume()
+        myResume._name = "Sifiso Khuzwayo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.add(job2);
+
+
+        job1.Display();
+        job2.Display();
+
     }
 }
