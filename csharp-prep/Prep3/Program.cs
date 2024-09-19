75cb5c99e3aefb664ab413faa2b84ce4651068ed
basil-byui <khu23004@byui.edu>
2024-09-19 16:03:13 +0200

    prep 2-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("what is your magic number? ");
+        int valueFromUser = int.Parse(Console.ReadLine());
+        
+        int guess = -1;
+
+        while (guess != valueFromUser);
+        Console.Write("what is your guess? ");
+       
+
+        if (guess > valueFromUser)
+        {
+            Console.WriteLine("guess Lower");
+        }
+
+        else if (guess < valueFromUser)
+        {
+            Console.WriteLine("guess Higher");
+        }
+
+        else 
+        {
+            Console.WriteLine("Well done, You've guessed it right!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (To quit enter 0): ");
+            string userResponse = Console.ReadLine();   
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);    
+            }
+        }
+
+
+        // calculating the sum:
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("The sume is: " + sum);
     }
+
 }
